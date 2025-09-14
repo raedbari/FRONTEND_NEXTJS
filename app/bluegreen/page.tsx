0e57e70cb46c5750d05987826f4d2a4c23d864ab
raedbari <raedbari203@gmail.com>
2025-09-14 17:30:28 +0300

    page_bluegreen

---
+++ app/bluegreen/page.tsx
@@ -0,0 +1,7 @@
+// app/bluegreen/page.tsx
+"use client";
+import BlueGreenActions from "../components/BlueGreenActions";
+
+export default function BlueGreenPage() {
+  return <BlueGreenActions />;
+}
